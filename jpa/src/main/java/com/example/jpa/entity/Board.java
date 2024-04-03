25169c73eaea9e0cfd2c36bbf7010b6430661469
DongWan <dlehddhks125@naver.com>
2024-04-03 17:48:19 +0900

    2024-04-03

---
+++ jpa/src/main/java/com/example/jpa/entity/Board.java
@@ -0,0 +1,38 @@
+package com.example.jpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+public class Board {
+
+    @SequenceGenerator(name = "board_seq_gen", sequenceName = "board_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "board_seq_gen")
+    @Id
+    private Long id;
+
+    @Column(nullable = true, length = 100)
+    private String title;
+
+    @Column(nullable = true, length = 1500)
+    private String content;
+
+    @Column(nullable = true, length = 50)
+    private String writer;
+}
